327bce1c8007e24b4c6e567c54df5a15b081859e
edrisBagheri <edris.bagheri.rm@gmail.com>
2023-06-13 16:45:13 -0700

    modify the existing dfs for one target and source to a algorithem that can find the path from multiple sources(outlets) to single target(inlet pipe)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 class PathFinder():
-    def __init__(self, source,target, maze):
-        self.source = source
+    def __init__(self, sources, target, maze):
+        self.sources = sources
         self.target = target
         self.path = []
         # Define the directions (up, down, left, right)
@@ -10,30 +11,28 @@ class PathFinder():
         self.maze = maze
         self.num_rows = len(maze)
         self.num_cols = len(maze[0])
+        self.layout = []
         # Create a visited matrix to keep track of visited cells
         self.visited = [[False for _ in range(self.num_cols)] for _ in range(self.num_rows)]
 
 
-    def find_path(self):
-        return self.dfs(self.source[0], self.source[1])
+    def find_path(self, start):
+        return self.dfs(start[0], start[1])
 
-    # Helper function to check if a given position is valid
+
     def is_valid(self, row, col):
-        return 0 <= row < self.num_rows and 0 <= col < self.num_cols and self.maze[row][col] == 0 and not self.visited[row][col]
+        #check if a given position is inside the maze
+        return 0 <= row < self.num_rows and 0 <= col < self.num_cols and self.maze[row][col] == 0
 
         # Helper function to perform DFS and find all paths
 
     def dfs(self, row, col):
         path = []
-        # Create a visited matrix to keep track of visited cells
-        self.visited
-        num_rows = self.num_rows
-        num_cols = self.num_cols
         directions = self.directions
 
         path.append((row, col))
         self.visited[row][col] = True
-        if (row, col) == (self.target[0], self.target[1]):
+        if (row, col) == (self.target[0], self.target[1]) or (row, col) in self.layout:
             return True
 
         path_is_finded = False
@@ -42,20 +41,22 @@ class PathFinder():
             dead_end = True
             for drow, dcol in directions:
                 new_row, new_col = row + drow, col + dcol
-
                 if self.is_valid(new_row, new_col):
-                    self.visited[new_row][new_col] = True
-                    if (new_row, new_col) == (self.target[0], self.target[1]):
-                        path_is_finded = True
-                    # add the node to the path
-                    path.append((new_row, new_col))
-                    row, col = new_row, new_col
-                    dead_end = False
-                    break
+                    if not self.visited[new_row][new_col] or (new_row, new_col) in self.layout:
+                        # once we have more that one path in layout the that are visited also are valid
+                        self.visited[new_row][new_col] = True
+                        if (new_row, new_col) == (self.target[0], self.target[1]) or (new_row, new_col) in self.layout:
+                            path_is_finded = True
+                        # add the node to the path
+                        path.append((new_row, new_col))
+                        row, col = new_row, new_col
+                        dead_end = False
+                        break
             if dead_end:
                 path.pop()
                 row, col = path[-1][0], path[-1][1]
-        # save the path in object
+        # add path to layout
+        self.layout.extend(path)
         return path
 
 
@@ -80,18 +81,16 @@ def create_maze():
     return maze
 
 
-def draw_path(path):
+def draw_path(paths):
 
     # Extract x and y values from coordinates
-    x = [coord[1] for coord in path]
-    y = [coord[0] for coord in path]
+    for p in paths:
+        x = [coord[1] for coord in p]
+        y = [coord[0] for coord in p]
 
-    # Plot the polyline
-    plt.plot(x, y, marker='o', linestyle='-', color='blue')
+        # Plot the polyline
+        plt.plot(x, y, marker='o', linestyle='-', color='blue')
 
-    # Set plot limits
-    plt.xlim(min(x) - 1, max(x) + 1)
-    plt.ylim(min(y) - 1, max(y) + 1)
 
     # Add labels and title
     plt.xlabel('X')
@@ -101,16 +100,54 @@ def draw_path(path):
     # Display the plot
     plt.show()
 
+def sort_sources(sources, target):
+    # sort targest regarding the distance in decending manner
+    # we like to find the path from farest target to source
+    # and reast of source branch of from that main route
+    dis = np.array(sources) - np.array(target)
+    sources_dis = [(np.linalg.norm(i), idx) for idx, i in enumerate(dis)]
+    sources_dis = sorted(sources_dis, reverse=True)
+    sorted_sources = [i[1] for i in sources_dis]
+    sources_coord_dic = {i: tuple(sources[i]) for i in sorted_sources}
+    coord_sources_dic = {tuple(sources[i]): i for i in sorted_sources}
+    return sources_coord_dic, coord_sources_dic
+
+def find_juction_placement(layout, coords,  target_coord_dic, coord_target_dic):
+    print(layout, coords)
+
+
+def finding_piping_layout(sources,target, maze):
+     # this dictionaries will be sufull for general purpose application and findin the neighbouring target more efficiently
+     sources_coord_dic, coord_sources_dic = sort_sources(sources,target)
+     path_finder = PathFinder(sources, target, maze)
+     paths = []
+     for source  in  range(len(sources)):
+        coords = sources_coord_dic[source]
+        path = path_finder.find_path(coords)
+        paths.append(path)
+
+     draw_path(paths)
+     return paths
+
+
+
+
+
 
 def main():
     maze = create_maze()
-    test_path_finder = PathFinder((0,4), (3,6),maze)
-    path = test_path_finder.find_path()
-    draw_path(path)
+    sources = [(3,6), (3,2)]
+    target = (0,4)
+    paths = finding_piping_layout(sources, target, maze)
+    # test_path_finder = PathFinder([(3,6)], np.array([0,4]) , maze)
+
+    # path = test_path_finder.find_path((3,6))
+    # draw_path(path)
 
     # print(test_path_finder.find_path())
 
 
 if __name__ == '__main__':
     main()
+
 
