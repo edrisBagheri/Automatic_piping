51cdc2bc525e30c52378544ccdf7534163eb2cc2
edrisBagheri <edris.bagheri.rm@gmail.com>
2023-06-15 08:56:39 -0700

    add networkX module and setup the graph, which represent the geometry, the first attempt is low resolution result, which make some simplification in geometry to make the graph smaller
     build_graph function is added which traslate the maze into graph. nodes and edges of graph are build based information of maze,

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import networkx as nx
 import visualization
 # from collections import deque
 
@@ -9,7 +10,7 @@ class Node():
 
 
 class PathFinder():
-    def __init__(self, sources, target, maze):
+    def __init__(self, sources, target, maze, scheme):
         self.sources = sources
         self.target = target
         # Define the directions (north(z+), south(z-), right(x+), left(x-), up(y+), down(y-))
@@ -23,6 +24,8 @@ class PathFinder():
         # Create a visited matrix to keep track of visited cells
         self.visited = np.zeros((self.num_rows, self.num_cols, self.num_elev ), dtype=bool)
         self.enforce_connection_direction()
+        self.scheme = scheme
+
 
     def enforce_connection_direction(self):
         # consider a obstacle in maze all directions except node (inlet/outlet) connection
@@ -43,8 +46,14 @@ class PathFinder():
 
 
     def find_path(self, start):
-        # return self.dfs(start[0], start[1], start[2])
-        return self.bfs(start[0], start[1], start[2])
+        if self.scheme == 0:
+            return self.dfs(start[0], start[1], start[2])
+        elif self.scheme == 1:
+            return self.bfs(start[0], start[1], start[2])
+        else:
+            return self.performant_path_finding(start[0], start[1], start[2])
+
+
 
 
     def is_valid(self, row, col, elev):
@@ -128,6 +137,61 @@ class PathFinder():
         self.layout.extend(path)
         return path
 
+    def performant_path_finding(self, row, col, elev):
+        graph, coord_node_map, node_coord_map = self.build_graph(self.maze)
+        source = coord_node_map[(row, col, elev)]
+        target = coord_node_map[self.target.coord]
+
+        path_nodes = nx.shortest_path(graph, source=source, target=target)
+
+        path_coords = [node_coord_map[i] for i in path_nodes]
+
+        return path_coords
+
+
+    def build_graph(self, maze):
+        # Get the number of vertices in the graph
+        num_row = maze.shape[0]
+        num_col = maze.shape[1]
+        num_elev = maze.shape[2]
+        # Create an empty graph
+        graph = nx.Graph()
+        node_coord_map = {}
+        coord_node_map = {}
+
+        node_id = 0
+        for i in range(maze.shape[0]):
+            for j in range(maze.shape[1]):
+                for k in range(maze.shape[2]):
+                    node_coord_map[node_id] = (i, j, k)
+                    coord_node_map[(i, j, k)] = node_id
+                    node_id += 1
+
+        # define edges
+        directions = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
+        edges = []
+        node_edge_map = {}
+        edge_node_map = {}
+
+        edge_id = 0
+        for node, coord in node_coord_map.items():
+            if maze[coord[0], coord[1], coord[2]] == 0:
+                for drow, dcol, delev in directions:
+                    new_row, new_col, new_elev = coord[0] + drow, coord[1] + dcol, coord[2] + delev
+                    if 0 <= new_row < num_row and 0 <= new_col < num_col and 0 <= new_elev < num_elev:
+                        if maze[new_row, new_col, new_elev] == 0:
+                            second_node = coord_node_map[(new_row, new_col, new_elev)]
+                            edges.append((node, second_node))
+                            edge_node_map[edge_id] = (node, second_node)
+                            node_edge_map.setdefault(node, []).append(edge_id)
+                            node_edge_map.setdefault(second_node, []).append(edge_id)
+                            edge_id += 1
+
+        for e in edges:
+            graph.add_edge(e[0], e[1])
+
+        return graph, coord_node_map, node_coord_map
+
 
 def create_maze():
 
@@ -139,26 +203,7 @@ def create_maze():
     for box in boxes:
         maze[box[0]:box[1], box[2]:box[3], box[4]:box[5]] = 1
 
-    #desplay maze
 
-
-    # for now manually creat a 2D maze
-    # maze = [
-    #         [0, 0, 0, 0, 0],
-    #         [0, 1, 0, 1, 0],
-    #         [0, 0, 0, 0, 0],
-    #         [0, 1, 1, 1, 0],
-    #         [0, 0, 0, 1, 0],
-    #         [0, 1, 0, 0, 0]
-    #     ]
-    # maze = [
-    #         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #         [0, 1, 1, 1, 1, 1, 1, 1, 0],
-    #         [0, 1, 0, 0, 0, 0, 0, 1, 0],
-    #         [0, 1, 0, 1, 0, 1, 0, 1, 0],
-    #         [0, 1, 1, 1, 0, 1, 1, 1, 0],
-    #         [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    #     ]
     return maze
 
 
@@ -185,7 +230,9 @@ def find_juction_placement(layout, coords,  target_coord_dic, coord_target_dic):
 def finding_piping_layout(sources,target, maze):
      # this dictionaries will be sufull for general purpose application and findin the neighbouring target more efficiently
      sources_coord_dic = sort_sources(sources,target)
-     path_finder = PathFinder(sources, target, maze)
+
+     # scheme 0: dfs, 1: bfs, 2: a_star
+     path_finder = PathFinder(sources, target, maze, scheme=2)
 
      paths = []
      for source  in  range(len(sources)):
@@ -205,18 +252,47 @@ def cal_material_bill(paths):
         total += len(p)-1
     return total
 
-def main():
+def setput_problem_1():
+    # creat geometry
     maze = create_maze()
-    sources = [Node((0,0,1),(2, 0, 3)), Node((0,0,1),(4, 0, 3))]
-    target = Node((0,0,-1),(0,4,5))
+    maze = create_maze()
+    sources = [Node((0, 0, 1), (2, 0, 3)), Node((0, 0, 1), (4, 0, 3))]
+    target = Node((0, 0, -1), (0, 4, 5))
+    return maze, sources, target
+
+def setput_problem_2():
+    # create maze(adjaicency matrix) that represent the geometry
+    maze = np.zeros((24, 30, 24), dtype=np.int32)
+    # -----------------------------------------------------------------------------
+    # geometry consist of diffrent boxes as an obstacle to be subtractet from domain
+    # each box ned a boundig box
+    boxes = [( 0,  8,  0, 30,  0, 12),
+             ( 8, 19,  3, 27,  0, 12),
+             ( 3, 19,  3,  6, 12, 24),
+             ( 3, 19, 24, 27, 12, 24),
+             ( 3,  6,  6, 24, 12, 20),
+             ( 6, 12, 11, 19, 12, 20),
+             (16, 19,  6, 24, 12, 24),
+             ( 3,  6,  6, 12, 12, 24),
+             ( 3,  6, 18, 24, 12, 24)
+             ]
+    for box in boxes:
+        maze[box[0]:box[1], box[2]:box[3], box[4]:box[5]] = 1
+    sources = [Node((0, 0, 1), (9, 9, 13)), Node((0, 0, 1), (9, 23, 13))]
+    target = Node((-1, 0, 0), (23, 14, 5))
+    return maze, sources, target
+
+def main():
+    # maze,sources, target = setput_proble_1()
+    maze, sources, target = setput_problem_2()
     paths, t_connections = finding_piping_layout(sources, target, maze)
     # test_path_finder = PathFinder(sources, (0, 4, 5), maze)
-    total_pipe_lenght = cal_material_bill(paths)
+    # total_pipe_lenght = cal_material_bill(paths)
     # path = test_path_finder.find_path((3, 0, 3))
     # paths= []
     # paths.append(path)
     print('t connection are occure at',  t_connections)
-    print('total pipe lenght:', total_pipe_lenght)
+    # print('total pipe lenght:', total_pipe_lenght)
     visualization.draw_paths_3D(paths)
 
 
