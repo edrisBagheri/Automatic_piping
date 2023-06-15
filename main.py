3b7b30fe052f98e647938d32c2c865afa77dffca
edrisBagheri <edris.bagheri.rm@gmail.com>
2023-06-14 17:03:10 -0700

    add modified bfs algorithm to find the shortest path,
    add functionality to enforce the direction of connections
    
    add visualization module to show layout of pipes

--- main.py
+++ main.py
@@ -1,66 +1,147 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import visualization
+# from collections import deque
+
+class Node():
+    def __init__(self, direct=(1, 0, 0), coord=(0,0,0)):
+        self.direct = direct
+        self.coord = coord
+
 
 class PathFinder():
     def __init__(self, sources, target, maze):
         self.sources = sources
         self.target = target
-        self.path = []
-        # Define the directions (up, down, left, right)
-        self.directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        # Define the directions (north(z+), south(z-), right(x+), left(x-), up(y+), down(y-))
+        self.directions = [(0, 0, 1), (0, 0, -1), (1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0)]
         self.maze = maze
-        self.num_rows = len(maze)
-        self.num_cols = len(maze[0])
+        self.num_rows = maze.shape[0]
+        self.num_cols = maze.shape[1]
+        self.num_elev = maze.shape[2]
+        self.t_connections = []
         self.layout = []
         # Create a visited matrix to keep track of visited cells
-        self.visited = [[False for _ in range(self.num_cols)] for _ in range(self.num_rows)]
+        self.visited = np.zeros((self.num_rows, self.num_cols, self.num_elev ), dtype=bool)
+        self.enforce_connection_direction()
+
+    def enforce_connection_direction(self):
+        # consider a obstacle in maze all directions except node (inlet/outlet) connection
+        connection_nodes = []
+        connection_nodes.extend(self.sources)
+        connection_nodes.append(self.target)
+        for s in connection_nodes:
+            coord = s.coord
+            for drow, dcol, delev in self.directions:
+                new_row, new_col, new_elev = coord[0] + drow, coord[1] + dcol, coord[2] + delev
+                if (new_row, new_col, new_elev) != (coord[0] + s.direct[0], coord[1] + s.direct[1], coord[2] + s.direct[2]):
+                    if self.is_valid(new_row, new_col, new_elev):
+                        self.maze[new_row, new_col, new_elev] = 1
+
+
+
+
 
 
     def find_path(self, start):
-        return self.dfs(start[0], start[1])
+        # return self.dfs(start[0], start[1], start[2])
+        return self.bfs(start[0], start[1], start[2])
 
 
-    def is_valid(self, row, col):
+    def is_valid(self, row, col, elev):
         #check if a given position is inside the maze
-        return 0 <= row < self.num_rows and 0 <= col < self.num_cols and self.maze[row][col] == 0
+        return 0 <= row < self.num_rows and 0 <= col < self.num_cols and 0 <= elev < self.num_elev and self.maze[row, col, elev] == 0
 
         # Helper function to perform DFS and find all paths
 
-    def dfs(self, row, col):
+
+    def bfs(self, row, col, elev):
+
+        target_coord = self.target.coord
+        directions = self.directions
+        start = ( row, col, elev)
+        # queue = deque([(start, [])])
+        queue = [(start, [])]
+        visited = set([start])
+        self.visited[row, col, elev] = True
+
+        while queue:
+            # current, path = queue.popleft()
+            current, path = queue[0][0], queue[0][1]
+            queue.pop(0)
+            row, col, elev = current
+
+            if current == target_coord or current in self.layout:
+
+                if self.layout:
+                    path += [current]
+                    self.layout.extend(path)
+                else:
+                    path += [current]
+                    self.layout.extend(path)
+                    self.t_connections.append(current)
+                return path
+
+            for drow, dcol, delev in directions:
+                # nx, ny = x + dx, y + dy
+                new_row, new_col, new_elev = row + drow, col + dcol, elev + delev
+                if self.is_valid(new_row, new_col, new_elev):
+                    if not self.visited[new_row, new_col, new_elev] or (new_row, new_col, new_elev) in self.layout:
+                        queue.append(((new_row, new_col, new_elev), path + [current]))
+                        visited.add((new_row, new_col, new_elev))
+
+
+
+
+    def dfs(self, row, col, elev):
         path = []
         directions = self.directions
-
-        path.append((row, col))
-        self.visited[row][col] = True
-        if (row, col) == (self.target[0], self.target[1]) or (row, col) in self.layout:
+        target_coord = (self.target[0], self.target[1], self.target[2])
+        path.append((row, col, elev))
+        self.visited[row, col, elev] = True
+        if (row, col, elev) == target_coord or (row, col, elev) in self.layout:
             return True
 
         path_is_finded = False
 
         while (path_is_finded == False):
             dead_end = True
-            for drow, dcol in directions:
-                new_row, new_col = row + drow, col + dcol
-                if self.is_valid(new_row, new_col):
-                    if not self.visited[new_row][new_col] or (new_row, new_col) in self.layout:
+            for drow, dcol, delev in directions:
+                new_row, new_col , new_elev = row + drow, col + dcol, elev + delev
+                if self.is_valid(new_row, new_col, new_elev):
+                    if not self.visited[new_row, new_col, new_elev] or (new_row, new_col, new_elev) in self.layout:
                         # once we have more that one path in layout the that are visited also are valid
-                        self.visited[new_row][new_col] = True
-                        if (new_row, new_col) == (self.target[0], self.target[1]) or (new_row, new_col) in self.layout:
+                        self.visited[new_row, new_col, new_elev] = True
+                        new_coord = (new_row, new_col, new_elev)
+
+                        if new_coord == target_coord or (new_row, new_col, new_elev) in self.layout:
                             path_is_finded = True
                         # add the node to the path
-                        path.append((new_row, new_col))
-                        row, col = new_row, new_col
+                        path.append((new_row, new_col, new_elev))
+                        row, col, elev = new_row, new_col, new_elev
                         dead_end = False
                         break
             if dead_end:
+                # backtracking
                 path.pop()
-                row, col = path[-1][0], path[-1][1]
+                row, col, elev = path[-1][0], path[-1][1], path[-1][2]
         # add path to layout
         self.layout.extend(path)
         return path
 
 
 def create_maze():
+
+    maze = np.zeros((6,6,6), dtype=np.int32)
+    # -----------------------------------------------------------------------------
+    # geometry consist of diffrent boxes as an obstacle to be subtractet from domain
+    # each box ned a boundig box
+    boxes = [(0,6,0,2,0,3)]
+    for box in boxes:
+        maze[box[0]:box[1], box[2]:box[3], box[4]:box[5]] = 1
+
+    #desplay maze
+
+
     # for now manually creat a 2D maze
     # maze = [
     #         [0, 0, 0, 0, 0],
@@ -70,47 +151,32 @@ def create_maze():
     #         [0, 0, 0, 1, 0],
     #         [0, 1, 0, 0, 0]
     #     ]
-    maze = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 1, 1, 1, 1, 0],
-            [0, 1, 0, 0, 0, 0, 0, 1, 0],
-            [0, 1, 0, 1, 0, 1, 0, 1, 0],
-            [0, 1, 1, 1, 0, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ]
+    # maze = [
+    #         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #         [0, 1, 1, 1, 1, 1, 1, 1, 0],
+    #         [0, 1, 0, 0, 0, 0, 0, 1, 0],
+    #         [0, 1, 0, 1, 0, 1, 0, 1, 0],
+    #         [0, 1, 1, 1, 0, 1, 1, 1, 0],
+    #         [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    #     ]
     return maze
 
 
-def draw_path(paths):
-
-    # Extract x and y values from coordinates
-    for p in paths:
-        x = [coord[1] for coord in p]
-        y = [coord[0] for coord in p]
-
-        # Plot the polyline
-        plt.plot(x, y, marker='o', linestyle='-', color='blue')
 
 
-    # Add labels and title
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('pipe_route')
-
-    # Display the plot
-    plt.show()
 
 def sort_sources(sources, target):
     # sort targest regarding the distance in decending manner
-    # we like to find the path from farest target to source
-    # and reast of source branch of from that main route
-    dis = np.array(sources) - np.array(target)
+    # we like to find the path from closest target to source
+    # since we are using BFS, the closer we are to target the faster algorithm works
+
+    sources_coords = np.array([i.coord for i in sources])
+    dis = np.array(sources_coords) - np.array(target.coord)
     sources_dis = [(np.linalg.norm(i), idx) for idx, i in enumerate(dis)]
-    sources_dis = sorted(sources_dis, reverse=True)
+    # sources_dis = sorted(sources_dis, reverse=True)
     sorted_sources = [i[1] for i in sources_dis]
-    sources_coord_dic = {i: tuple(sources[i]) for i in sorted_sources}
-    coord_sources_dic = {tuple(sources[i]): i for i in sorted_sources}
-    return sources_coord_dic, coord_sources_dic
+    sources_coord_dic = {idx: tuple(sources_coords[i]) for idx, i in enumerate(sorted_sources)}
+    return sources_coord_dic
 
 def find_juction_placement(layout, coords,  target_coord_dic, coord_target_dic):
     print(layout, coords)
@@ -118,31 +184,41 @@ def find_juction_placement(layout, coords,  target_coord_dic, coord_target_dic):
 
 def finding_piping_layout(sources,target, maze):
      # this dictionaries will be sufull for general purpose application and findin the neighbouring target more efficiently
-     sources_coord_dic, coord_sources_dic = sort_sources(sources,target)
+     sources_coord_dic = sort_sources(sources,target)
      path_finder = PathFinder(sources, target, maze)
+
      paths = []
      for source  in  range(len(sources)):
         coords = sources_coord_dic[source]
         path = path_finder.find_path(coords)
         paths.append(path)
 
-     draw_path(paths)
-     return paths
+     # visualization.draw_paths_3D(paths)
+     return paths, path_finder.t_connections
 
 
 
 
-
+def cal_material_bill(paths):
+    total = 0
+    for p in paths:
+        total += len(p)-1
+    return total
 
 def main():
     maze = create_maze()
-    sources = [(3,6), (3,2)]
-    target = (0,4)
-    paths = finding_piping_layout(sources, target, maze)
-    # test_path_finder = PathFinder([(3,6)], np.array([0,4]) , maze)
+    sources = [Node((0,0,1),(2, 0, 3)), Node((0,0,1),(4, 0, 3))]
+    target = Node((0,0,-1),(0,4,5))
+    paths, t_connections = finding_piping_layout(sources, target, maze)
+    # test_path_finder = PathFinder(sources, (0, 4, 5), maze)
+    total_pipe_lenght = cal_material_bill(paths)
+    # path = test_path_finder.find_path((3, 0, 3))
+    # paths= []
+    # paths.append(path)
+    print('t connection are occure at',  t_connections)
+    print('total pipe lenght:', total_pipe_lenght)
+    visualization.draw_paths_3D(paths)
 
-    # path = test_path_finder.find_path((3,6))
-    # draw_path(path)
 
     # print(test_path_finder.find_path())
 
